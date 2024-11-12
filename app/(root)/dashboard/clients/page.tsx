2947629cb625373566f8e76d39635714b0da76b6
MsingathiMat <msingathihlazo0@gmail.com>
2024-11-12 07:29:46 +0200

    UseClient

--- app/(root)/dashboard/clients/page.tsx
+++ app/(root)/dashboard/clients/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react'
 import { MttTabContainer, MttTabContent, MttTabList, MttTabTrigger } from '@/components/mtt/components/MttTabs'
 import FormAddClient from './FormAddClient'
