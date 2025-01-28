90aea56f97fecc3390cadd3f3d55d2337468d2ba
MsingathiMat <msingathihlazo0@gmail.com>
2025-01-28 18:56:01 +0200

    ENV-Changed

--- app/(auth)/signin/FormSignin.tsx
+++ app/(auth)/signin/FormSignin.tsx
@@ -102,7 +102,7 @@ const OriginalForm = ({ Utilities }: { Utilities: UtilitiesProp }) => {
               label="Email"
               className=""
             />
-<>No findUnique</>
+
             <MttTextField
               readOnly={readOnly}
               Icon="lock"
