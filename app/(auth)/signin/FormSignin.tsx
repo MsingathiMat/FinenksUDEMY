8a4c635eba6e62d988e9edb1efafe9db8d46cfac
MsingathiMat <msingathihlazo0@gmail.com>
2025-01-28 18:37:45 +0200

    No findUnique

--- app/(auth)/signin/FormSignin.tsx
+++ app/(auth)/signin/FormSignin.tsx
@@ -102,7 +102,7 @@ const OriginalForm = ({ Utilities }: { Utilities: UtilitiesProp }) => {
               label="Email"
               className=""
             />
-<>Mongo DB</>
+<>No findUnique</>
             <MttTextField
               readOnly={readOnly}
               Icon="lock"
